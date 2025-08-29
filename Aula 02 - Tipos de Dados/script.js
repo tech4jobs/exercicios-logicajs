fd1418b46f2032ede0882a172397180aecf7f852
tech4jobs <instrutortech@tech4meonline.com.br>
2025-08-29 10:58:07 -0300

    Commit da correção do exercício 2 e 3

---
+++ Aula 02 - Tipos de Dados/script.js
@@ -0,0 +1,11 @@
+//2 -  Exibindo valores de variáveis no console
+let conteudo;
+console.log(conteudo);
+conteudo = "Lógica de programação";
+console.log(conteudo);
+
+// 3 - Mudando valores
+let corFavorita = "Azul";
+console.log(corFavorita);
+corFavorita = "Vermelho";
+console.log(corFavorita);
